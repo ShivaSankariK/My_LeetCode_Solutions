f4da49c1b6f45a0d277f3d99af9829f714e883cd
Shiva Sankari Krishnamoorthy <90498617+ShivaSankariK@users.noreply.github.com>
2021-09-11 13:13:31 +0530

    Time: 128 ms (50.64%), Space: 44 MB (65.08%) - LeetHub

---
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let longest = 0;
+    let seen = {};
+    let start = 0;
+    for(var i=0; i< s.length; i++){
+        let char = s[i];
+        if(seen[char]){
+            start = Math.max(start, seen[char]);
+        }
+        longest = Math.max(longest, i-start+1);
+        seen[char] = i+1;
+    }
+    return longest;
+};
