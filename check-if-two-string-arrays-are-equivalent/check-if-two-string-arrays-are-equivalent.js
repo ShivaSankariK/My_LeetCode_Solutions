e5d4a9274300c95e8bcf822cfecd4a383b860c9c
Shiva Sankari Krishnamoorthy <90498617+ShivaSankariK@users.noreply.github.com>
2021-09-11 13:43:43 +0530

    Time: 91 ms (17.74%), Space: 38.4 MB (93.57%) - LeetHub

---
+++ check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+   var str1 = word1.join("");
+    var str2 = word2.join("");
+    return str1 === str2;
+};
