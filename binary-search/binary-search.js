a8aa8622f970350413394cc2bfd5e6355747bf3a
Shiva Sankari Krishnamoorthy <90498617+ShivaSankariK@users.noreply.github.com>
2021-09-12 14:55:23 +0530

    Time: 133 ms (7.89%), Space: 42 MB (87.87%) - LeetHub

---
+++ binary-search/binary-search.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let min = 0;
+    let max = nums.length-1;
+    if(nums.length === 1){
+        if(target === nums[0]){
+            return min;
+        }
+    }
+    while(min <= max){
+        let mid = Math.floor((min+max)/2);
+        if(target === nums[mid]){
+            return mid;
+        } else if(target < nums[mid]){
+            max = mid-1;
+        } else {
+            min = mid+1;
+        }
+    }
+    return -1;
+};
