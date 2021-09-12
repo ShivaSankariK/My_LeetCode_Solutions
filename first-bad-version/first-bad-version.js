705824a44121b9da2aee0aaa4d1db8d350bd1c82
Shiva Sankari Krishnamoorthy <90498617+ShivaSankariK@users.noreply.github.com>
2021-09-12 15:09:06 +0530

    Time: 102 ms (23.81%), Space: 38.2 MB (86.85%) - LeetHub

---
+++ first-bad-version/first-bad-version.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+    let min = 1;
+    let max = n;
+        let result = n;
+    while(min <= max){
+        let mid = Math.floor((min+max)/2);
+        if(isBadVersion(mid)){
+            result = mid;
+           max = mid -1;
+        } else {
+            min = mid+1;
+        }
+    }
+    return result;
+};
+};
