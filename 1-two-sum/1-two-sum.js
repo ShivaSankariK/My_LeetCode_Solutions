a2ffb04b9480262eba0a3a72412f9e212b9918cb
Shiva Sankari Krishnamoorthy <90498617+ShivaSankariK@users.noreply.github.com>
2022-02-27 12:41:35 +0530

    Time: 59 ms (99.35%), Space: 42.8 MB (24.29%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var obj ={};
+    for(var i=0; i<nums.length; i++){
+        let diff = target - nums[i];
+        var val = nums[i];
+        if(obj[diff] !== undefined){
+            return [obj[diff], i];
+        } else{
+            obj[val] = i;
+        }
+    }
+};
