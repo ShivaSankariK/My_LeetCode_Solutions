b09dfafa75ae7a5a46eb9ae46320820e180e1167
Shiva Sankari Krishnamoorthy <90498617+ShivaSankariK@users.noreply.github.com>
2025-05-13 14:56:24 +0530

    Time: 3 ms (21.49%), Space: 54.2 MB (55.46%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -4,23 +4,23 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    let result = "";
-    let i= a.length - 1;
-    let j = b.length - 1;
-    let carry = 0;
-    while(i >=0 || j>=0){
-        let sum = carry;
-        if(i >=0){
-            sum += a[i--]- '0';
-        }
-        if(j >=0){
-            sum += b[j--] - '0';
-        }
-        result = sum % 2 + result;
-        carry = parseInt(sum/2);
+   var result = "";
+   let i = a.length -1;
+   let j= b.length - 1;
+   let carry = 0;
+   while(i>=0 || j>=0){
+    let sum = carry;
+    if(i>=0){
+        sum += a[i--] - '0';
     }
-    if(carry > 0){
-        result = 1+result;
+    if(j>=0){
+        sum += b[j--] - '0';
     }
-    return result;
+    result = sum % 2 + result;
+    carry = parseInt(sum/2);
+   }
+   if(carry > 0){
+    result = 1+result;
+   }
+   return result;
 };
