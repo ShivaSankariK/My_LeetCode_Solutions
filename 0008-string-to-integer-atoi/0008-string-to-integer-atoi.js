a8f83c097056341283bd60ac242a0281e7c3ccd2
Shiva Sankari Krishnamoorthy <90498617+ShivaSankariK@users.noreply.github.com>
2025-05-19 23:06:29 +0530

    Time: 1 ms (89.33%), Space: 57.6 MB (33.09%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function (str) {
+    const converted = parseInt(str);
+
+    if (converted > 2147483647) {
+        return 2147483647;
+    } else if (converted < -2147483648) {
+        return -2147483648;
+    }
+
+    return isNaN(converted) ? 0 : converted;
+};
