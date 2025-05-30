e6dbb32a7a62c07384f64764b24bb254952c31bd
Shiva Sankari Krishnamoorthy <90498617+ShivaSankariK@users.noreply.github.com>
2025-05-30 22:22:24 +0530

    Time: 7 ms (95.65%), Space: 61.6 MB (10.56%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+     if (!ransomNote) return true;
+    if (!magazine) return false;
+
+    for (let i = 0; i < ransomNote.length; i++) {
+        if (!magazine.includes(ransomNote[i])) return false;
+
+        magazine = magazine.replace(ransomNote[i], "");
+    }
+
+    return true
+};
